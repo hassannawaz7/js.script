bc7bbf42aa061b33e2c6b1bd42dcafda9c3fd3e7
hassannawaz7 <mhassannawaz001@gmail.com>
2024-10-18 10:23:24 +0000

    math&num

---
+++ 02-basic/num&math.js
@@ -0,0 +1,34 @@
+// const score= 400
+// console.log(score);
+
+
+// //const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().lenght);
+// console.log(balance.toFixsd(2));
+
+// const otherNumber=123.4543
+// console.log(balance.toPrecision());
+
+
+// const Number=10000000
+// console.log(balance.tolocalstring(en-IN));
+
+//////////Math/////////////
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.6));
+// console.log(Math.round(4.5));
+// console.log(Math.min(4, 5 ,9, 2));
+// console.log(Math.max(4, 5 ,9, 2));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min=10
+const max = 20
+console.log(math.floor(MATHrandom()*(max-min+1))+min )
